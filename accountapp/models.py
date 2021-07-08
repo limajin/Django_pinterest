38586a2774566c53b043f17a35b1ae751659c1a9
aj8782 <dkwls8782@likelion.org>
2021-07-08 11:53:30 +0900

    7/8 commit 2

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HelloWorld(models.Model):
+    # 클래스를 상속받아서 클래스 만듦
+
+
+    text = models.CharField(max_length=255, null=False)

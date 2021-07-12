a74d14f215cb67801944f1bbb024b18c9fd5933d
aj8782 <dkwls8782@likelion.org>
2021-07-12 11:10:33 +0900

    7/12 commit1

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,13 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+        # input 안에 POST안에 input_text
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'text': temp})
+        # 유저에게 입력받은 것을 그대로 출력
 
     else:
         return render(request, 'accountapp/hello_world.html',

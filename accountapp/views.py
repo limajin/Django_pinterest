d38077d9dcbcdcfc9e77ed428678abdf193e2f60
aj8782 <dkwls8782@likelion.org>
2021-07-13 17:06:58 +0900

    7/13 commit1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -26,3 +29,14 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    # reverse_lazy 주소를 역추적하는 건 같지만
+    # 함수형 view에서는 바로 불러주면 되지만
+    # 객체가 생성되고나서 따로 불러줘야해서 reverse_lazy
+    # 성공했을 때 넘어가는 url
+    template_name = 'accountapp/create.html'

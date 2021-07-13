12d2ddb47dcb8bbfcc446dbd0a46004598be50d9
aj8782 <dkwls8782@likelion.org>
2021-07-13 18:12:16 +0900

    7/13 commit3

--- gis_1/settings.py
+++ gis_1/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 # path(__file__) 은 settings.py의 파일 경로를 의미함
 # parent 부모의 경로
@@ -149,3 +151,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')

2b56c059971f164bac76b8790911b40e154370c7
aj8782 <dkwls8782@likelion.org>
2021-08-19 12:07:42 +0900

    8/19 commit3

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
@@ -41,10 +42,17 @@ class AccountCreateView(CreateView):
     # 성공했을 때 넘어가는 url
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list,
+                                        **kwargs)
 
 
 has_ownership = [login_required, account_ownership_required]

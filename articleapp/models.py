19fdcb9ef4c7bcf62f12c3953af7eddf82bf16b2
aj8782 <dkwls8782@likelion.org>
2021-08-19 11:18:37 +0900

    8/19 commit1

--- articleapp/models.py
+++ articleapp/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     # 연결된 유저객체가 삭제 됐을 때 작성자 미상의 글로 남겨두겠다
+
+    project = models.ForeignKey(Project,on_delete=models.SET_NULL,
+                                related_name='article', null=True)
+
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)

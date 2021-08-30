729114b64f514c8b9522c4c621f25312218a0392
aj8782 <dkwls8782@likelion.org>
2021-08-30 11:21:33 +0900

    8/30 commit1

--- articleapp/models.py
+++ articleapp/models.py
@@ -12,7 +12,8 @@ class Article(models.Model):
     # 연결된 유저객체가 삭제 됐을 때 작성자 미상의 글로 남겨두겠다
 
     project = models.ForeignKey(Project,on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
+    # 여기서 null = True는 디비상 널 트루임 실제 입력을 받지않아도는 아님
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
@@ -20,6 +21,7 @@ class Article(models.Model):
     # 긴문자열 TextField
 
     created_at = models.DateField(auto_now_add=True, null=True)
+    # 여기서 null = True는 디비상 널 트루임 실제 입력을 받지않아도는 아닌다.
     # 시간 정보 자동 입력되게
 
     like = models.IntegerField(default=0)
